d3badf577014ccb238409c35d6f9699ac5724eb4
Jake Barnhill <jakebarnhill67@gmail.com>
2021-06-26 08:44:31 -0400

    Initial commit

---
+++ src/main/java/dev/warse/warsemenus/WarseMenus.java
@@ -0,0 +1,13 @@
+package dev.warse.warsemenus;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class WarseMenus {
+
+    private static JavaPlugin main;
+
+    public static void init(JavaPlugin plugin) {
+        main = plugin;
+    }
+
+}
